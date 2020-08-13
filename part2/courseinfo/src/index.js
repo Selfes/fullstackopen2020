87ebc7322ebba2ce4711b23a4c4cd2a49324247e
Kenny He <reshiram1997@gmail.com>
2020-08-13 15:10:32 +0200

    copy courseinfo from Part1 to Part2

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course.name}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  // Inspired by map reduce :)
+  return (
+    <>
+      <p>Number of exercises {props.course.parts.reduce((acc, elem) => acc + elem.exercises,0)}</p>
+    </>
+  )
+}
+
+const App = () => {
+  // const-definitions
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+// Template
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+//
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+ReactDOM.render(<App />, document.getElementById('root'))
