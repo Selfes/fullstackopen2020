996a620245177ba1cffd70f3c481455d41474363
Kenny He <reshiram1997@gmail.com>
2020-08-13 15:15:24 +0200

    took index.js from solutions

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,45 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const Header = (props) => {
+const Header = ({ course }) => {
   return (
-    <>
-      <h1>{props.course.name}</h1>
-    </>
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
   )
 }
 
 const Part = (props) => {
   return (
-    <>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </>
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
   )
 }
 
-const Content = (props) => {
+const Content = ({ course }) => {
   return (
-    <>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
-    </>
-  )
-}
-
-const Total = (props) => {
-  // Inspired by map reduce :)
-  return (
-    <>
-      <p>Number of exercises {props.course.parts.reduce((acc, elem) => acc + elem.exercises,0)}</p>
-    </>
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
   )
 }
 
 const App = () => {
-  // const-definitions
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -66,32 +59,5 @@ const App = () => {
     </div>
   )
 }
-
-// Template
-// const App = () => {
-//   const course = 'Half Stack application development'
-//   const part1 = 'Fundamentals of React'
-//   const exercises1 = 10
-//   const part2 = 'Using props to pass data'
-//   const exercises2 = 7
-//   const part3 = 'State of a component'
-//   const exercises3 = 14
-//
-//   return (
-//     <div>
-//       <h1>{course}</h1>
-//       <p>
-//         {part1} {exercises1}
-//       </p>
-//       <p>
-//         {part2} {exercises2}
-//       </p>
-//       <p>
-//         {part3} {exercises3}
-//       </p>
-//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-//     </div>
-//   )
-// }
 
 ReactDOM.render(<App />, document.getElementById('root'))
