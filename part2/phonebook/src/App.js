0c218d67872823ff9abb252cbe102cc753b1a888
Kenny He <14095577+Selfes@users.noreply.github.com>
2020-12-08 02:13:01 +0100

    Part2 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    const person = { name: newName }
+    const person = { name: newName, number: newNumber }
 
     if (persons.some(elem => elem.name === newName)) {
       alert(`${newName} is already added to phonebook`)
@@ -27,11 +28,14 @@ const App = () => {
           name: <input value={newName} onChange={(event) => {setNewName(event.target.value)}} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(event) => {setNewNumber(event.target.value)}} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
