52db9e18c8befa99c5184ce5afa9dac11e38789e
Kenny He <14095577+Selfes@users.noreply.github.com>
2020-12-20 02:53:56 +0100

    Part2 2.13

--- part2/datacountries/src/components/Countries.js
+++ part2/datacountries/src/components/Countries.js
@@ -5,6 +5,19 @@ const Countries = (props) => {
 
     const showCountries = props.filter ? props.countries.filter((country) => country.name.toLowerCase().includes(props.filter.toLowerCase())) : []
 
+    // inspired by https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+    const showCountry = (country) => {
+      var showIt = document.getElementById("info-" + country);
+      var button = document.getElementById("btn-" + country);
+      if (showIt.style.display === "none") {
+        showIt.style.display = "block";
+        button.innerHTML = "hide";
+      } else {
+        showIt.style.display = "none";
+        button.innerHTML = "show";
+      }
+    };
+
     if (showCountries.length === 1) {
         const country = showCountries[0];
         return ( <Country country={country} /> )
@@ -16,7 +29,19 @@ const Countries = (props) => {
 
     return (
         <div>
-          {showCountries.map((country) => <p key={country.name}>{country.name}</p>)}
+        {showCountries.map((country) => (
+            <span key={country.name}>
+              <p key={country.name}>
+                {country.name}
+                <button key={"btn-"+country.name} id={"btn-"+country.name} onClick={() => showCountry(country.name)}>
+                  show
+                </button>
+              </p>
+              <div style={{display: "none"}} key={"info-" + country.name} id={"info-" + country.name}>
+                <Country key={"country-" + country.name} country={country} />
+              </div>
+            </span>
+        ))}
         </div>
     );
 }
