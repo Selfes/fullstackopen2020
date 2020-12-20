1bf8fd8e14e2b667e2c2f785f3a59e2f6269ad4b
Kenny He <14095577+Selfes@users.noreply.github.com>
2020-12-20 09:40:42 +0100

    Part2 2.15

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,37 +1,50 @@
-import React from 'react'
+import React from "react";
+import axios from "axios"
 
 const PersonForm = (props) => {
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    const person = { name: props.name, number: props.number };
 
-    const addPerson = (event) => {
-        event.preventDefault()
-      const person = { name: props.name, number: props.number }
-
-      if (props.persons.some(elem => elem.name === props.name)) {
-            alert(`${props.name} is already added to phonebook`)
-            return
-        }
-
-        props.addPerson(props.persons.concat(person))
-        props.nameSetter('')
-        props.numberSetter('')
+    if (props.persons.some((elem) => elem.name === props.name)) {
+      alert(`${props.name} is already added to phonebook`);
+      return;
     }
 
-    return (
-        <>
-          <form onSubmit={addPerson}>
-            <div>
-              name: <input value={props.name} onChange={(event) => {props.nameSetter(event.target.value)}} />
-            </div>
-            <div>
-              number: <input value={props.number} onChange={(event) => {props.numberSetter(event.target.value)}} />
-            </div>
-            <div>
-              <button type="submit">add</button>
-            </div>
-          </form>
-        </>
-    )
-}
+    props.addPerson(props.persons.concat(person));
+    props.nameSetter("");
+    props.numberSetter("");
+    return axios.post("http://localhost:3001/persons", person).then(response => response.data);
+  };
 
-export default PersonForm
+  return (
+    <>
+      <form onSubmit={addPerson}>
+        <div>
+          name:{" "}
+          <input
+            value={props.name}
+            onChange={(event) => {
+              props.nameSetter(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            value={props.number}
+            onChange={(event) => {
+              props.numberSetter(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default PersonForm;
